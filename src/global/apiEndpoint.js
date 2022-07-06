313f675614ae4512347f6be65a22adedc8a81d1c
Mochamad Rizky Cahya Diputra <moch.rizky.cahya@gmail.com>
2022-07-06 22:45:40 +0700

    Update size flag api endpoint

--- src/global/apiEndpoint.js
+++ src/global/apiEndpoint.js
@@ -2,7 +2,7 @@ import CONFIG from "./config";
 
 const API_ENDPOINT = {
   GET_RANDOM_USER: (count) => `${CONFIG.BASE_URL_RANDOM_USER}/api/?results=${count}`,
-  GET_FLAG_IMAGE: (countryCode) => `${CONFIG.BASE_URL_COUNTRY}/flags/${countryCode}.png`
+  GET_FLAG_IMAGE: (countryCode) => `${CONFIG.BASE_URL_COUNTRY}/48x36/${countryCode}.png`
 };
 
 export default API_ENDPOINT;
