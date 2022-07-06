afc9fb142df5b4571112c8de94b4d6e7c1329108
Mochamad Rizky Cahya Diputra <moch.rizky.cahya@gmail.com>
2022-07-06 22:46:42 +0700

    Add request user functionality

--- src/App.js
+++ src/App.js
@@ -1,6 +1,38 @@
+import { useEffect, useState } from "react";
+import axios from "axios";
+import API_ENDPOINT from "./global/apiEndpoint";
+import {
+  ListUser,
+  HeaderDashboard,
+} from "./components";
+
+import './App.css';
+
 function App() {
+  const [users, setUsers] = useState(null);
+  
+  useEffect(() => {
+    const getDataUser = async () => {
+      try {
+        const response = await axios.get(API_ENDPOINT.GET_RANDOM_USER(25));
+        
+        setUsers(response.data.results);
+      } catch (error) {
+        console.log(error);
+      }
+    };
+    
+    getDataUser();
+  }, []);
+  
   return (
-    <div>hello world</div>
+    <main className="member-dashboard">
+      <section className="member-dashboard__container">
+        <h1>Member Dashboard</h1>
+        <HeaderDashboard userData={users}/>
+        <ListUser users={users}/>
+      </section>
+    </main>
   );
 }
 
