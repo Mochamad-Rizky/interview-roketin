f7a8a28240f32d24ef16bdb870e137878f444af3
Mochamad Rizky Cahya Diputra <moch.rizky.cahya@gmail.com>
2022-07-06 22:48:22 +0700

    Add all components into export default object

---
+++ src/components/index.js
@@ -0,0 +1,5 @@
+export { default as ListUser } from './ListUser/ListUser';
+export { default as UserItem } from './UserItem/UserItem';
+export { default as HeaderDashboard } from './HeaderDashboard/HeaderDashboard';
+export { default as HeaderDashboardItem } from './HeaderDashboardItem/HeaderDashboardItem';
+export { default as Loading } from './Loading/Loading';
